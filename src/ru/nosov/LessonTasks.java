dfc1bbfd0f6c37425897936a7fdf544c0b8183a3
Nosov <Nosovid@gmail.com>
2021-07-07 22:49:58 +0300

    Finish task 4

--- src/ru/nosov/LessonTasks.java
+++ src/ru/nosov/LessonTasks.java
@@ -1,41 +1,40 @@
 package ru.nosov;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class LessonTasks {
-
-    public static void task1(int[] array) {
+    //Task1
+    public static String checkArraySorting(int[] array) {
 
         boolean state = true;
 
         for (int i = 0; i < array.length - 1; i++) {
             int index = i;
+            //System.out.println("i: " + array[i]);
+            //System.out.println("i+1: " + array[i+1]);
 
-
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[index]) {
-                    state = false;
-                    break;
-                } else {
-                    continue;
-                }
-
+            if (array[i] > array[i+1]) {
+                state = false;
+                break;
             }
-
-//            if (state = true) {
-//                System.out.println("OK");
-//            } else {
-//                System.out.println("Please, try again");
-//                break;
-//            }
-
         }
+        //System.out.println("State after FOR: " + state);
 
+        if (state == false) {
+            System.out.println("Please, try again");
+            return "Please, try again";
+        } else {
+            System.out.println("OK - array is sorted well");
+            return "OK";
+        }
     }
 
-
-    public static void task2() {
+    //Task2
+    public static void enterArrayFromKeyboard() {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите длину массива");
@@ -51,8 +50,8 @@ public class LessonTasks {
         System.out.println("Результат: " + Arrays.toString(array));
     }
 
-
-    public static int[] task3(int[] array) {
+    //Task3
+    public static int[] reverseArrayValues(int[] array) {
         int first = array[0];
         int last = array[array.length-1];
 
@@ -63,20 +62,71 @@ public class LessonTasks {
 
 
     }
+    //Task4
+    public static int findUniqueArrayValue(int[] array) {
+        int firstUniqueValue = 0;
+        int uniqueIndex;
+        int[] uniqueIndexList = new int[array.length];
 
-    public static void task4() {
+        for (int i = 0; i < array.length; i++) {
+            Arrays.fill(uniqueIndexList, 0);
 
+            for (int j = 0; j < array.length; j++) {
 
+                if (i != j && array[i] == array[j])
+                    uniqueIndexList[j] = 1;
+            }
+            //Посмотреть массив сравнения уникальности для каждого элемента исходного массива
+            //System.out.println(Arrays.toString(uniqueIndexList));
+
+            uniqueIndex = 0;
+
+            for (int k = 0; k < uniqueIndexList.length; k++) {
+
+                uniqueIndex += uniqueIndexList[k];
+
+            }
+            if (uniqueIndex == 0) {
+                firstUniqueValue = array[i];
+                break;
+            }
+        }
+        if (firstUniqueValue == 0)
+            System.out.println("No unique values in array");
+        return firstUniqueValue;
     }
 
-    public static void task5() {
+    //task5
+    public static int findFibonachi() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число N > 0 для расчета N-го числа Фибоначи");
+        int n = scanner.nextInt();
 
+
+        int[] array = new int[n];
+        int fibonachi = 0;
+
+        if (n > 0 && n != 0 && n != 1) {
+            for (int i = 0; i < n; i++) {
+                array[i] = i;
+            }
+            fibonachi = array[n-2]+ array[n-1];
+
+        }else if (n == 0) {
+            fibonachi = 0;
+
+        }else  if(n ==1) {
+            fibonachi = 1;
+
+        }
+        return fibonachi;
     }
 
+    //task6
     public static void task6() {
 
     }
-
+    //task7
     public static void task7() {
 
     }
--- src/ru/nosov/Main.java
+++ src/ru/nosov/Main.java
@@ -1,19 +1,18 @@
 package ru.nosov;
 
-import java.sql.Array;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         //Algorithm Task1
         //int[] AT1array = {1, 2, 3, 4, 2, 7, 8, 10};
         //System.out.println(Arrays.toString(AlgorithmTask.removeDigit(AT1array, 2)));
 
         //Algorithm Task 2
         //int[] AT2array = new int[10];
-       // AlgorithmTask.Randomizer(AT2array, -20, 20);
+        // AlgorithmTask.Randomizer(AT2array, -20, 20);
         //AlgorithmTask.Print(AT2array);
         //System.out.println(AlgorithmTask.PositiveIntegerSum(AT2array));
 
@@ -22,21 +21,35 @@ public class Main {
 
         //Lesson task 1
         System.out.println("Task1:");
-//        int[] LT1array = {1, 2, 5, 4, 5, 6, 7, 8, 9, 10};
-//        LessonTasks.task1(LT1array);
+        int[] example1 = {1, 2, 6, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(example1));
+        LessonTasks.checkArraySorting(example1);
 
         //Lesson task 2
         System.out.println("Task2:");
-//        LessonTasks.task2();
+//        LessonTasks.enterArrayFromKeyboard();
 
         //Lesson task 3
         System.out.println("Task3:");
-//        int[] LT3array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//        System.out.println("Array original: " + Arrays.toString(LT3array));
-//        System.out.println("Mixed array: " + Arrays.toString(LessonTasks.task3(LT3array)));
+        int[] example3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Array original: " + Arrays.toString(example3));
+        System.out.println("Mixed array: " + Arrays.toString(LessonTasks.reverseArrayValues(example3)) +"\n");
 
         //Lesson task 4
         System.out.println("Task4:");
+        int[] example4 = {1, 2, 3, 2, 2, 4};
+        System.out.println("Array original: " + Arrays.toString(example4));
+        System.out.println(LessonTasks.findUniqueArrayValue(example4));
+
+        //Lesson task 5
+        System.out.println("Task5:");
+        //System.out.println(LessonTasks.findFibonachi());
+
+        //Lesson task 6
+        System.out.println("Task6:");
+
+
+
 
 
 
